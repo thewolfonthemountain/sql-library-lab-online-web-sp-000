9331386579cbeb11d2382a8c64791deb16e6e518
Sean Langton <slangton1@gmail.com>
2019-01-03 03:02:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+    FROM characters
+    JOIN series on series.id = characters.series_id
+    WHERE species = "human"
+    GROUP BY series.title
+    ORDER BY count(series.title)
+    LIMIT 1
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
