caa5c266e4fc974bc3e20d3d8482bc21f32ee245
Sean Langton <slangton1@gmail.com>
2019-01-03 02:35:44 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -21,3 +21,12 @@ CREATE TABLE books (
   year INTEGER,
   series_id INTEGER
 );
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,  
+  series_id INTEGER
+);
