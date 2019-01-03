8a3b62dd903d58d049680a775659a513d355f0c8
Sean Langton <slangton1@gmail.com>
2019-01-03 02:54:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, count(species) FROM characters GROUP BY species ORDER BY count(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
