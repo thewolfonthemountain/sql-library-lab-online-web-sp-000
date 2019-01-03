3ce3012691ed0676a5cda96db6e48a95081151bc
Sean Langton <slangton1@gmail.com>
2019-01-03 02:36:29 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -27,6 +27,12 @@ CREATE TABLE characters (
   name TEXT,
   motto TEXT,
   species TEXT,
-  author_id INTEGER,  
+  author_id INTEGER,
   series_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
 );
